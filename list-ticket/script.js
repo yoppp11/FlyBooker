604d9f6d5db39f89f3c03c02d764b390c318d599
yoppp11 <yopinur040903@gmail.com>
2025-02-04 01:21:27 +0700

    fix logic search ticket

--- list-ticket/script.js
+++
@@ -1,8 +0,0 @@
-
-const urlParam = new URLSearchParams(window.location.search)
-
-const asal = urlParam.get('asal')
-const tujuan = urlParam.get('tujuan')
-const tanggal = urlParam.get('tanggal')
-
-console.log(asal, tujuan, tanggal)
